9e64c8fae11bd0e537f017db1e8fcf134391ce66
Martin Wickham <spexguy070@gmail.com>
2014-03-01 10:40:09 -0600

    save Monsters!

--- HackAThon/PathfinderDMAssistant/src/edu/jainwickhamphillips/MonsterBuilderTwo.java
+++ HackAThon/PathfinderDMAssistant/src/edu/jainwickhamphillips/MonsterBuilderTwo.java
@@ -1,12 +1,16 @@
 package edu.jainwickhamphillips;
 
-import android.app.Activity;
+import java.sql.SQLException;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MonsterBuilderTwo extends Activity {
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+
+public class MonsterBuilderTwo extends OrmLiteBaseActivity<Database> {
 
 	private final int[] HIDAMAGE = new int[]{4,7,10,13,16,20,25,30,35,40,45,50,55,60,65,70,80,90,100,110,120};
 	private final int[] LOWDAMAGE = new int[]{3,5,7,9,12,15,18,22,26,30,33,37,40,45,48,52,60,67,75,82,90};
@@ -38,6 +42,26 @@ public class MonsterBuilderTwo extends Activity {
 		percentage/=100;
 		return(int)(hi*percentage + low*(1-percentage)+.5);
 		
+	}
+	
+	public void save(View v) {
+        SeekBar damage=(SeekBar)findViewById(R.id.seekBar_Damageslider);
+        SeekBar variance=(SeekBar)findViewById(R.id.seekBar_Varianceslider);
+        SeekBar initiative=(SeekBar)findViewById(R.id.seekBar_Initiativeslider);
+        curMonster.setVariance(variance.getProgress());
+        curMonster.setInitiative(getModifier(0,10,initiative.getProgress())); //in the future get a function to determine min an max initiative values
+        curMonster.setDamage(getModifier(LOWDAMAGE[curMonster.getCR()-1],HIDAMAGE[curMonster.getCR()-1],damage.getProgress()));
+        
+        curMonster.setName("TEST_NAME!!!");
+        
+        try {
+			this.getHelper().getDao(Monster.class).create(curMonster);
+		} catch (SQLException e) {
+			throw new RuntimeException("Failed to save Monster", e);
+		}
+        
+        Intent intent = new Intent(this, MonsterListView.class);
+        startActivity(intent);
 	}
 	
 
