cbc67066b726d662ddcbfa807508b49f80509045
Nikhil <nikhiljain15@yahoo.com>
2014-03-01 10:33:25 -0600

    Bugs in building monster, mostly fixed.
    Database seems to work

--- HackAThon/PathfinderDMAssistant/src/edu/jainwickhamphillips/Database.java
+++ HackAThon/PathfinderDMAssistant/src/edu/jainwickhamphillips/Database.java
@@ -12,8 +12,8 @@ import com.j256.ormlite.table.TableUtils;
 
 public class Database extends OrmLiteSqliteOpenHelper {
 
-	private static final String DATABASE_NAME = "PathfinderDMAssist.db";
-	private static final int DATABASE_VERSION = 4;
+	private static final String DATABASE_NAME = "PathfinderDMAssist2.db";
+	private static final int DATABASE_VERSION = 1;
 	
 	public Database(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION,
@@ -23,13 +23,13 @@ public class Database extends OrmLiteSqliteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db, ConnectionSource conn) {
 		try {
-			//TableUtils.createTableIfNotExists(conn, Campaign.class);
-			//Log.d("error","database start");
+			TableUtils.createTableIfNotExists(conn, Campaign.class);
+			Log.d("error","database start");
 			TableUtils.createTableIfNotExists(conn, dieGroup.class);
-			//TableUtils.createTableIfNotExists(conn, Encounter.class);
+			TableUtils.createTableIfNotExists(conn, Encounter.class);
 			TableUtils.createTableIfNotExists(conn, Monster.class);
-			//TableUtils.createTableIfNotExists(conn, MonsterGroup.class);
-			//TableUtils.createTableIfNotExists(conn, PlayerCharacter.class);
+			TableUtils.createTableIfNotExists(conn, MonsterGroup.class);
+			TableUtils.createTableIfNotExists(conn, PlayerCharacter.class);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
--- HackAThon/PathfinderDMAssistant/src/edu/jainwickhamphillips/diceCalculator.java
+++ HackAThon/PathfinderDMAssistant/src/edu/jainwickhamphillips/diceCalculator.java
@@ -7,7 +7,7 @@ public  class diceCalculator {
 	private static double totalDamage=0;
 	private static int diceType;
 	private static int d12,d10,d8,d6,d4;
-	
+
 	public static dieGroup calculateDice(int damage, double variance){
 		d12=d10=d8=d6=d4=0;
 		totalDamage=0;
@@ -15,7 +15,7 @@ public  class diceCalculator {
 		int constantDamage;
 		variance/=100;
 		diceDamage=(int)(variance*damage +.5);
-		
+
 		if(damage<3 || diceDamage<3){ //if damage/diceDamage is less than 3, no dice can add to equal it.
 			constantDamage=damage;
 			return(new dieGroup(constantDamage,d12,d10,d8,d6,d4));
@@ -28,34 +28,60 @@ public  class diceCalculator {
 			if(temp>=1)
 				temp=1;
 			primaryDieNum+=getDieNum(diceDamage, temp); //ensures primaryDieNum has a value != 0
-			
+
 		}
 		int primaryDieType=diceType;
 		totalDamage+=(primaryDieNum*(1+diceType)/2.0);
-		
-		
-		int secondaryDieNum=getDieNum((int)(diceDamage-totalDamage),variance+.2); //+.2
+
+
+		boolean earlyExit=false;
+		temp=.2;
+		if(variance+temp>1){
+			temp=1;
+			earlyExit=true;
+		}
+
+		int secondaryDieNum=getDieNum((int)(diceDamage-totalDamage),temp); //+.2
 		int secondaryDieType=diceType;
 		totalDamage+=(secondaryDieNum*(1+diceType)/2.0); //secondary
-		temp=.4;
-		if(secondaryDieNum==0){ //bounds check assign vars
-			secondaryDieNum=getDieNum((int)(diceDamage-totalDamage),variance+temp);
+
+		if(!earlyExit){
+
+
+			temp=.4;
+			if(variance+temp>1){
+				temp=1;
+				earlyExit=true;
+			}
+			if(secondaryDieNum==0){ //bounds check assign vars
+				secondaryDieNum=getDieNum((int)(diceDamage-totalDamage),temp);
+				secondaryDieType=diceType;
+				totalDamage+=(secondaryDieNum*(1+diceType)/2.0);
+			}
+
+
+			if(!earlyExit){
+				temp=.6; //bounds check assign vars
+				if(variance+temp>1)
+					temp=1;
+				if(secondaryDieNum==0){
+					secondaryDieNum=getDieNum((int)(diceDamage-totalDamage),temp);
+					secondaryDieType=diceType;
+					totalDamage+=(secondaryDieNum*(1+diceType)/2.0);
+				}
+			}
+
 		}
-		temp=.6; //bounds check assign vars
-		if(secondaryDieNum==0){
-			secondaryDieNum=getDieNum((int)(diceDamage-totalDamage),variance+temp);
-		}
-		
-		
-	
+
+
 		assignValue(primaryDieType, primaryDieNum);
 		assignValue(secondaryDieType, secondaryDieNum);
 		constantDamage=damage-(int)totalDamage;
-		
+
 
 
 		return new dieGroup(constantDamage,d12,d10,d8,d6,d4);
-		
+
 	}
 	private static int getDieNum(int damage, double variance){
 		int[] numberOfDice = new int[5];
@@ -65,7 +91,7 @@ public  class diceCalculator {
 		numberOfDice[3]=(int)(damage/((1+dieGroup.D6)/2.0));		//d6
 		numberOfDice[4]=(int)(damage/((1+dieGroup.D4)/2.0));		//d4
 		int index=(int)(variance*4.999) ; //gives me best fit. 
-		
+
 		if(index==0)
 			diceType=12;
 		else if(index==1)
@@ -76,9 +102,9 @@ public  class diceCalculator {
 			diceType=6;
 		else if(index==4)
 			diceType=4;
-		
+
 		return numberOfDice[index];
-		
+
 	}
 	private static void assignValue(int diceType, int numDice){
 		if(diceType==12)
@@ -92,5 +118,5 @@ public  class diceCalculator {
 		if(diceType==4)
 			d4+=numDice;
 	}
-	
+
 }
